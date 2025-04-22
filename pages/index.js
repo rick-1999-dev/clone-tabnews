540f6a6e2685705663d965c65c46c4ef9017b3d8
rick-1999-dev <henrique12antunesferreira@gmail.com>
2025-04-22 20:39:57 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function  Home() {
-    return <h1>Meu amor eu amo você!! e se você me ama da uma risadinha (:
+    return <h1>Meu amor eu amo você!! e se você me ama da uma risadinha (: s2 te amo mais que tudo
     </h1>
 }
 
